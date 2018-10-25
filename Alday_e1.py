ac157abe1cfdaf601ef8eecb09d849d91f915bf1
GeraldineAlday <aldaygeraldine@gmail.com>
2018-10-25 22:09:49 +0800

    exercise

---
+++ Alday_e1.py
@@ -0,0 +1,2 @@
+print("I am Geraldine Alday \n My spirit animal is Koala \n because I love to sleep \n When not in school, I love to read books \n I dream of being a/an Doctor in the future ")
+
--- hello.py
+++ hello.py
@@ -1,2 +1,5 @@
 print ("Hello World")
 print("My name is {}.I am {} y/0.".format("Geraldine",17))
+
+print("ADNU",2018,end="")
+print("CSNHS")
